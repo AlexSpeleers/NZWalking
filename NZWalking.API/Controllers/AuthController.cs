6cb9fad480f8a4005c8e1fee0f46aac9ce0f5787
Alex <alexsieger@gmail.com>
2024-07-08 05:00:06 +0200

    auth controller added

---
+++ NZWalking.API/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NZWalking.API.Models.DTO;
+
+namespace NZWalking.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(UserManager<IdentityUser> userManager) : ControllerBase
+    {
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDTO registerRequestDTO)
+        {
+            IdentityUser identityUser = new IdentityUser()
+            {
+                UserName = registerRequestDTO.Username,
+                Email = registerRequestDTO.Username
+            };
+            var identityResult = await userManager.CreateAsync(identityUser, registerRequestDTO.Password);
+            if (identityResult.Succeeded)
+            {
+                if (registerRequestDTO.Roles != null && registerRequestDTO.Roles.Any())
+                {
+                    identityResult = await userManager.AddToRolesAsync(identityUser, registerRequestDTO.Roles);
+                    if(identityResult.Succeeded) 
+                    {
+                        return Ok("User was registred succesfuly.");
+                    }
+                }
+            }
+            return BadRequest("Something went wrong.");
+        }
+    }
+}
