37305f7bd581470934f6bf381164919b8eee8791
Alex <alexsieger@gmail.com>
2024-07-08 17:51:15 +0200

    Register/login actions and JWT token

--- NZWalking.API/Controllers/AuthController.cs
+++ NZWalking.API/Controllers/AuthController.cs
@@ -13,24 +13,41 @@ namespace NZWalking.API.Controllers
         [Route("Register")]
         public async Task<IActionResult> Register([FromBody] RegisterRequestDTO registerRequestDTO)
         {
-            IdentityUser identityUser = new IdentityUser()
+            IdentityUser identityUser = new()
             {
                 UserName = registerRequestDTO.Username,
                 Email = registerRequestDTO.Username
             };
             var identityResult = await userManager.CreateAsync(identityUser, registerRequestDTO.Password);
-            if (identityResult.Succeeded)
+            if (!identityResult.Succeeded)
             {
                 if (registerRequestDTO.Roles != null && registerRequestDTO.Roles.Any())
                 {
                     identityResult = await userManager.AddToRolesAsync(identityUser, registerRequestDTO.Roles);
-                    if(identityResult.Succeeded) 
+                    if (identityResult.Succeeded)
                     {
                         return Ok("User was registred succesfuly.");
                     }
                 }
             }
             return BadRequest("Something went wrong.");
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDTO loginRequestDTO)
+        {
+            IdentityUser? identityUser = await userManager.FindByEmailAsync(loginRequestDTO.UserName);
+            if (identityUser is not null)
+            {
+
+                var checkPasswordResult = await userManager.CheckPasswordAsync(identityUser, loginRequestDTO.Password);
+                if (checkPasswordResult)
+                {
+                    return Ok();
+                }
+            }
+            return BadRequest("Username or password is are incorrect");
         }
     }
 }
