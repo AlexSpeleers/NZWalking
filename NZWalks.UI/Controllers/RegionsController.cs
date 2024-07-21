22d94861d11a9e1a41d95a8f21a555c1b30bd7b7
Alex <alexsieger@gmail.com>
2024-07-21 22:55:27 +0200

    post regions WEB API

--- NZWalks.UI/Controllers/RegionsController.cs
+++ NZWalks.UI/Controllers/RegionsController.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.UI.Models;
 using NZWalks.UI.Models.DTO;
+using System.Text;
+using System.Text.Json;
 
 namespace NZWalks.UI.Controllers
 {
     public class RegionsController(IHttpClientFactory httpClientFactory) : Controller
     {
-        public async Task <IActionResult> Index()
+        public async Task<IActionResult> Index()
         {
             List<RegionDTO> response = new List<RegionDTO>();
             try
@@ -20,6 +23,32 @@ namespace NZWalks.UI.Controllers
             {
             }
             return View(response);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(AddRegionViewModel model)
+        {
+            var client = httpClientFactory.CreateClient();
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://localhost:7260/api/regions"),
+                Content = new StringContent(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json")
+            };
+            await client.SendAsync(httpRequestMessage);
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var response = await httpResponseMessage.Content.ReadFromJsonAsync<RegionDTO>();
+            if (response is not null)
+            {
+                return RedirectToAction("Index", "Regions");
+            }
+            return View();
         }
     }
 }
---
+++ NZWalks.UI/Models/AddRegionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks.UI.Models
+{
+    public class AddRegionViewModel
+    {
+        [Display(Name=nameof(Code))]
+        public string Code { get; set; }
+
+        [Display(Name = nameof(Name))]
+        public string Name { get; set; }
+
+        [Display(Name = nameof(RegionImageUrl))]
+        public string RegionImageUrl { get; set; }
+    }
+}
